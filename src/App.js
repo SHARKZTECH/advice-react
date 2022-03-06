315c2c3b42679637424be0a6a8ba5f9965031a77
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-03-06 13:21:06 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import {Container,Card,Button} from "react-bootstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state={advice:''}
+  componentDidMount(){
+     this.getAdvice()
+  }
+  getAdvice=()=>{
+    fetch("https://api.adviceslip.com/advice")
+    .then((res)=>res.json())
+    .then(({slip})=>this.setState({advice:slip.advice}))
+    .catch(error=>console.error(error))
+  }
+  render(){
+    const {advice}=this.state;
+    return(
+      <Container>
+          <Card className="text-center m-5">
+            <Card.Header>EAGLEYE ADVICE</Card.Header>
+            <Card.Body>
+              <Card.Title>{advice}</Card.Title>
+              
+              <Button variant="primary" onClick={()=>this.getAdvice()}>GIVE ME ADVICE</Button>
+            </Card.Body>
+          </Card>
+      </Container>
+    )
+  }
 }
 
 export default App;
